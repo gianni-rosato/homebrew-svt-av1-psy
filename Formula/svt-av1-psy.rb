01d26008db3c0b0ac40ef2fdaafc821358ed3bfe
Gianni <giannirosato@proton.me>
2025-01-14 01:32:17 -0500

    fix: failure to build on Apple Silicon due to upstream issue

--- Formula/svt-av1-psy.rb
+++ Formula/svt-av1-psy.rb
@@ -1,7 +1,7 @@
 class SvtAv1Psy < Formula
   desc "Perceptual AV1 Encoder"
   homepage "https://svt-av1-psy.com"
-  url "https://github.com/gianni-rosato/svt-av1-psy/archive/refs/tags/v2.2.1-B.tar.gz"
+  url "https://github.com/gianni-rosato/svt-av1-psy/archive/refs/tags/v2.3.0-A.tar.gz"
   sha256 "96d4fbec8fb1ef679f5445e47a93ae9f8622cb181571e24deac8f80344927a44"
   license "BSD-3-Clause"
   head "https://github.com/gianni-rosato/svt-av1-psy.git", branch: "master"
@@ -10,7 +10,14 @@ class SvtAv1Psy < Formula
   depends_on "nasm" => :build
 
   def install
-    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_INSTALL_RPATH=#{rpath}", "-DSVT_AV1_LTO=ON", *std_cmake_args
+    extra_cmake_args = %W[-DCMAKE_INSTALL_RPATH=#{rpath}]
+
+    # Explicitly disable ARM NEON I8MM extension on Apple Silicon: upstream
+    # build script attempts to detect CPU features via compiler flags, which
+    # are stripped by brew's compiler shim. The M1 chip does not support the
+    # I8MM extension (hw.optional.arm.FEAT_I8MM).
+    extra_cmake_args << "-DENABLE_NEON_I8MM=OFF" if OS.mac?
+    system "cmake", "-S", ".", "-B", "build", *extra_cmake_args, "-DSVT_AV1_LTO=ON", *std_cmake_args
     system "cmake", "--build", "build", "--parallel"
     system "cmake", "--install", "build"
   end
